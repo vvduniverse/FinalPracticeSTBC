2269a02b7fbe595d19faedb77ca51848ea656012
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-08 10:46:09 +0300

    Initial Commit

---
+++ FinalPracticeSTBC/FinalPracticeSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalPracticeSTBCApp.swift
+//  FinalPracticeSTBC
+//
+//  Created by Vahtee Boo on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FinalPracticeSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
